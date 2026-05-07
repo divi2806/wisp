0fccb8c186a12297705e04adac7e1bbbde2e7658
NinjasPyajamas <divyansh242805@gmail.com>
2026-05-08 04:26:21 +0530

    implemented prediction market

---
+++ app/home/prediction-market/page.tsx
@@ -0,0 +1,5 @@
+import { PredictionMarketTerminal } from "@/components/prediction/PredictionMarketTerminal";
+
+export default function PredictionMarketPage() {
+  return <PredictionMarketTerminal />;
+}
