b19c85ce2c11ef4700880e0ee01d60cdb5d2fbe4
NinjasPyajamas <divyansh242805@gmail.com>
2026-05-08 23:47:47 +0530

    added wallet connect and user onboarding

--- app/home/prediction-market/page.tsx
+++ app/home/prediction-market/page.tsx
@@ -1,5 +1,13 @@
 import { PredictionMarketTerminal } from "@/components/prediction/PredictionMarketTerminal";
+import WispPageBar from "@/components/WispPageBar";
 
 export default function PredictionMarketPage() {
-  return <PredictionMarketTerminal />;
+  return (
+    <div className="flex flex-col h-screen">
+      <WispPageBar />
+      <div className="flex-1 min-h-0">
+        <PredictionMarketTerminal />
+      </div>
+    </div>
+  );
 }
